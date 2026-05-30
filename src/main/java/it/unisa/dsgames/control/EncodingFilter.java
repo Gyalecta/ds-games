4f379079c11fe1a7dca239ddc5e0869b4752711b
Gyalecta <domenico.avino@proton.me>
2026-05-30 12:45:31 +0200

    Inserimento file per testing tomcat

---
+++ src/main/java/it/unisa/dsgames/control/EncodingFilter.java
@@ -0,0 +1,33 @@
+package it.unisa.dsgames.control;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Filter che imposta l'encoding UTF-8 su tutte le richieste e risposte.
+ */
+public class EncodingFilter implements Filter {
+
+    private String encoding = "UTF-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String enc = filterConfig.getInitParameter("encoding");
+        if (enc != null && !enc.isEmpty()) {
+            this.encoding = enc;
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding(encoding);
+        response.setCharacterEncoding(encoding);
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // nulla da fare
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>DS Games</title>
+</head>
+<body>
+    <h1>DS Games - funziona!</h1>
+</body>
+</html>
